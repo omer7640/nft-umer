7ab855e10806d9d24fa70491b863c49db410325d
Umer <omerhamid.255@gmail.com>
2024-07-31 13:27:29 +0500

    updated files

--- src/components/marketPlace/NftCard.jsx
+++ src/components/marketPlace/NftCard.jsx
@@ -34,7 +34,7 @@ export default function NftCard({ nft }) {
           </div>
           <div className="btn-div">
             <button
-              onClick={value.deleteNft}
+              onClick={() => value.deleteNft(nft.id)}
               type="button"
               class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-1.5 me-1 mb-1 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
             >
